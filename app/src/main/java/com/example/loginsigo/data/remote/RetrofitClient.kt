987c2d0a473fd82f902a31f1aee44d3f4437caba
utm-ncpa <nelson.padilla@ut-morelia.edu.mx>
2025-12-09 09:06:27 -0600

    Se agrega documentación del código

--- app/src/main/java/com/example/loginsigo/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/loginsigo/data/remote/RetrofitClient.kt
@@ -1,21 +1,29 @@
-import com.example.loginsigo.data.remote.ApiService;
-
+import com.example.loginsigo.data.remote.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/**
+ * Objeto singleton para crear y configurar una instancia de Retrofit.
+ */
 object RetrofitClient {
-private const val BASE_URL = "http://189.206.96.198:8080/"
+    private const val BASE_URL = "http://189.206.96.198:8080/"
 
-// Creamos la instancia de Retrofit
-val retrofit: Retrofit by lazy {
-    Retrofit.Builder()
+    /**
+     * Instancia de Retrofit configurada con la URL base y un convertidor de Gson.
+     * La creación es diferida (lazy) para que se inicialice solo cuando se accede por primera vez.
+     */
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-}
+    }
 
-// Creamos la implementación de la interfaz de la API
-val apiService: ApiService by lazy {
-    retrofit.create(ApiService::class.java)
-}
+    /**
+     * Implementación de la interfaz [ApiService] para realizar las llamadas a la API.
+     * La creación es diferida (lazy) para que se inicialice solo cuando se accede por primera vez.
+     */
+    val apiService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
 }
